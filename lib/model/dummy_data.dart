a24ff7c77ad684695df886f30fb0d098047b284d
Zach Cervi <zachcervi@gmail.com>
2019-04-26 16:28:42 -0600

    added dummy data for posts

---
+++ lib/model/dummy_data.dart
@@ -0,0 +1,17 @@
+import 'post.dart';
+
+List<Post> getPosts() {
+  return [
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+    Post('iOS 12 Animation Tutorial', 'https://youtube.com', ['iOS', 'Mobile']),
+  ];
+}
---
+++ lib/model/post.dart
@@ -0,0 +1,7 @@
+class Post {
+  final String title;
+  final String url;
+  final List<String> tags;
+
+  Post(this.title, this.url, this.tags);
+}
