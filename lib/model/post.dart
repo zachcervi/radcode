885133d5f32d8d22c8950c3438f4f1e0cf864de1
Zach Cervi <zachcervi@gmail.com>
2019-04-27 20:13:28 -0600

    Connected to local api and pulling down data successfully

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
 class Post {
   final String title;
   final String url;
   final List<String> tags;
 
   Post(this.title, this.url, this.tags);
+
+  Post.fromJSON(Map<String, dynamic> json)
+      : title = json['title'] ?? '',
+        url = json['url'] ?? '',
+        tags = json['tags'].cast<String>() ?? [];
+
+  Map<String, dynamic> toJSON() => {'title': title, 'url': url, 'tags': tags};
+
+  static Future<List<Post>> fetchPosts() async {
+    try {
+      final response = await get('http://localhost:8080/posts');
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonPostList = jsonDecode(response.body);
+        List<Post> posts = [];
+        for (var x in jsonPostList) {
+          posts.add(Post.fromJSON(x));
+        }
+        return posts;
+      } else {
+        throw Exception('Failed to download data from the api');
+      }
+    } catch (ex) {
+      throw Exception(ex);
+    }
+  }
 }
--- lib/widgets/post_list.dart
+++ lib/widgets/post_list.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:radcode/model/dummy_data.dart';
+import 'package:radcode/model/post.dart';
 import 'post_card.dart';
 
 class PostList extends StatelessWidget {
   PostList() : super();
 
-  final posts = getPosts();
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i < posts.length) {
-            return buildPostCard(posts[i]);
-          }
-        },
-      ),
+    return FutureBuilder<List<Post>>(
+      future: Post.fetchPosts(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(16.0),
+              itemBuilder: (BuildContext _context, int i) {
+                if (i < snapshot.data.length) {
+                  return buildPostCard(snapshot.data[i]);
+                }
+              },
+            ),
+          );
+        } else if (snapshot.hasError) {
+          throw Exception(snapshot.error);
+        } else {
+          return Container(
+              padding: EdgeInsets.all(15), child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
