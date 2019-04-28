5ae0595918f7c1ad5adb959d63227f0cf19e1fda
Zach Cervi <zachcervi@gmail.com>
2019-04-27 19:19:52 -0600

    styled and added the tags in the table

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:radcode/model/post.dart';
 
+Widget _buildTags(List<String> tags) {
+  List<Widget> chips = [];
+  for (var i = 0; i < tags.length; i++) {
+    final chip = Expanded(
+        child: Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Chip(
+              label: Text(
+                tags[i],
+              ),
+              backgroundColor: Colors.blue,
+            )));
+    chips.add(chip);
+  }
+  return Row(children: chips);
+}
+
 Widget buildPostCard(Post post) {
   return Card(
       child: Container(
@@ -13,6 +30,7 @@ Widget buildPostCard(Post post) {
                 style: TextStyle(fontSize: 18),
               ),
               Text('iOS Android Mobile Flutter Development 2.0'),
+              _buildTags(post.tags)
             ],
           )));
 }
